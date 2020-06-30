05475ce035dadc334a5f8b0ff7222b8fb417f676
nashjunior <nashjuniorjj@gmail.com>
2020-06-30 10:25:51 -0300

    fixed: CalculadorDeClasses

--- java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ java-8/src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -1,5 +1,6 @@
 package com.challenge.desafio;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import com.challenge.annotation.Somar;
@@ -13,10 +14,10 @@ public class CalculadorDeClasses implements Calculavel {
     BigDecimal sum = BigDecimal.ZERO;
     for (Field field : classe.getClass().getDeclaredFields()) {
       if (isBigDecimal(field)) {
-        if (isSumAnnotation(field)) {
+        if (isSumOrSubAnnotation(field, Somar.class)) {
           field.setAccessible(true);
           try {
-            sum = sum.add((BigDecimal) field.get(classe));
+            sum = increment(sum, (BigDecimal) field.get(classe));
           } catch (IllegalArgumentException | IllegalAccessException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -33,10 +34,10 @@ public class CalculadorDeClasses implements Calculavel {
     BigDecimal sub = new BigDecimal(0);
     for (Field field : classe.getClass().getDeclaredFields()) {
       if (isBigDecimal(field)) {
-        if (isSubAnnotation(field)) {
+        if (isSumOrSubAnnotation(field,Subtrair.class)) {
           field.setAccessible(true);
           try {
-            sub = sub.add((BigDecimal) field.get(classe));
+            sub = increment(sub, (BigDecimal) field.get(classe));
           } catch (IllegalArgumentException | IllegalAccessException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -62,11 +63,12 @@ public class CalculadorDeClasses implements Calculavel {
     return field.getType().isAssignableFrom(BigDecimal.class);
   }
 
-  public Boolean isSumAnnotation (Field field){
-    return field.isAnnotationPresent(Somar.class);
+  public Boolean isSumOrSubAnnotation (Field field, Class <? extends Annotation> tipoAnnotation){
+    return field.isAnnotationPresent(tipoAnnotation);
   }
 
-  public Boolean isSubAnnotation (Field field){
-    return field.isAnnotationPresent(Subtrair.class);
+  public BigDecimal increment(BigDecimal sum, BigDecimal value){
+    return sum.add(value);
   }
+
 }
