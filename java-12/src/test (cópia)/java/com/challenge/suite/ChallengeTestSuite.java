a5e02f0d391bc14e04117d97d5b3758059add9ba
Nash Junior <nashjuniorjj@gmail.com>
2020-07-27 12:08:59 -0300

    added: 7th week aceleraDev

--- java-12/src/test (cópia)/java/com/challenge/suite/ChallengeTestSuite.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.suite;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({Oauth2Test.class})
-public class ChallengeTestSuite {
-}
